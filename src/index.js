64eac5845bef1b405a5a5355bc256d1cad9dda0d
KK Chen <kkchen@ebay.com>
2017-11-20 12:30:26 +0800

    Added axios and commander

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
 #!/usr/bin/env node
-console.log('Hello, world!');
+var program = require('commander');
+var axios = require('axios');
+
+axios.get('https://api.coinmarketcap.com/v1/ticker/')
+.then(function (response) {
+  console.log(response);
+})
+.catch(function (error) {
+  console.log(error);
+});
+
+// program
+//   .arguments('<coin>')
+//   .action(function(coin) {
+//     console.log('coin: %s', coin);
+//   })
+//   .parse(process.argv);
