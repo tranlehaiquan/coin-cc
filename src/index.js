4cf6b8f0c607267aa9bf284650a87cfa1788d5f8
KK Chen <kk@bichenkk.com>
2017-11-28 07:26:38 +0800

    Removed emoji for non-mac devices

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 #!/usr/bin/env node
-const program = require('commander');
-const axios = require('axios');
-const ora = require('ora');
-const cfonts = require('cfonts');
-const Table = require('cli-table2');
-const colors = require('colors');
-const humanize = require('humanize-plus');
+const program = require('commander')
+const axios = require('axios')
+const ora = require('ora')
+const cfonts = require('cfonts')
+const Table = require('cli-table2')
+const colors = require('colors')
+const humanize = require('humanize-plus')
+const os = require('os')
 
+const platform = os.platform() // linux, darwin, win32, sunos
+const supportEmoji = platform !== 'darwin'
 const list = val => val.split(',')
 
 program
@@ -15,7 +18,7 @@ program
   .option('-f, --find [symbol]', 'Find specific coin data with coin symbol (can be a comma seperated list)', list, [])
   .option('-t, --top [index]', 'Show the top coins ranked from 1 - [index] according to the market cap', null)
   .option('-H, --humanize [enable]', 'Show market cap as a humanized number, default true', true)
-  .parse(process.argv);
+  .parse(process.argv)
 
 const convert = program.convert.toUpperCase()
 const availableCurrencies = ['USD', 'AUD', 'BRL', 'CAD', 'CHF', 'CLP', 'CNY', 'CZK', 'DKK', 'EUR', 'GBP', 'HKD', 'HUF', 'IDR', 'ILS', 'INR', 'JPY', 'KRW', 'MXN', 'MYR', 'NOK', 'NZD', 'PHP', 'PKR', 'PLN', 'RUB', 'SEK', 'SGD', 'THB', 'TRY', 'TWD', 'ZAR']
@@ -45,7 +48,7 @@ const table = new Table({
   },
   head: ['Rank', 'Coin', `Price (${convert})`, 'Change (24H)', 'Change (1H)', `Market Cap (${convert})`].map(title => title.yellow),
   colWidths: [6, 14, 15, 15, 15, 20]
-});
+})
 
 cfonts.say('coinmon', {
   font: 'simple3d',
@@ -56,12 +59,12 @@ cfonts.say('coinmon', {
   lineHeight: 1,
   space: true,
   maxLength: '0'
-});
-const spinner = ora('Loading data').start();
+})
+const spinner = ora('Loading data').start()
 const sourceUrl = `https://api.coinmarketcap.com/v1/ticker/?limit=${top}&convert=${convert}`
 axios.get(sourceUrl)
 .then(function (response) {
-  spinner.stop();
+  spinner.stop()
   response.data
     .filter(record => {
       if (find.length > 0) {
@@ -76,11 +79,11 @@ axios.get(sourceUrl)
       const percentChange1h = record.percent_change_1h
       const textChange1h = `${percentChange1h}%`
       const change1h = percentChange1h ? (percentChange1h > 0 ? textChange1h.green : textChange1h.red) : 'NA'
-      const marketCap = record[`market_cap_${convert}`.toLowerCase()];
-      const displayedMarketCap = humanizeIsEnabled ? humanize.compactInteger(marketCap, 3) : marketCap;
+      const marketCap = record[`market_cap_${convert}`.toLowerCase()]
+      const displayedMarketCap = humanizeIsEnabled ? humanize.compactInteger(marketCap, 3) : marketCap
       return [
         record.rank,
-        `💰  ${record.symbol}`,
+        `${supportEmoji ? '💰  ' : ''}${record.symbol}`,
         record[`price_${convert}`.toLowerCase()],
         change24h,
         change1h,
@@ -89,13 +92,13 @@ axios.get(sourceUrl)
     })
     .forEach(record => table.push(record))
   if (table.length === 0) {
-    console.log('We are not able to find coins matching your keywords'.red);
+    console.log('We are not able to find coins matching your keywords'.red)
   } else {
     console.log(`Data source from coinmarketcap.com at ${new Date().toLocaleTimeString()}`)
-    console.log(table.toString());
+    console.log(table.toString())
   }
 })
 .catch(function (error) {
-  spinner.stop();
-  console.error('Coinmon is not working now. Please try again later.'.red);
-});
+  spinner.stop()
+  console.error('Coinmon is not working now. Please try again later.'.red)
+})
